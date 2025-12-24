27cf95d38629a0b6b5815f53635064e327514836
Muhammaddiyor Shokirov <muhammaddiyorshokirov72@gmail.com>
2025-12-24 13:50:15 +0500

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         applicationId = "uz.voiplay.tv"
-        minSdk = flutter.minSdkVersion
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
